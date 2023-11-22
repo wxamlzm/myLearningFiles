d7bae2464c05dd1e54c9d0b7534f15bf6becb130
wxamlzm <zd5024@163.com>
2023-11-22 16:04:06 +0800

    UPDATE：设法合并同pid的元素

---
+++ note/面试题/js/arrayGroup.js
@@ -0,0 +1,17 @@
+/*
+ * @Author: zd
+ * @Date: 2023-11-22 15:12:05
+ * @LastEditors: zd
+ * @LastEditTime: 2023-11-22 16:01:09
+ * @FilePath: \learningFiles\note\面试题\js\arrayGroup.js
+ * @Description: 列表转成树形结构
+ */
+
+let arr = [
+  { id: 1, name: '部门1', pid: 0 },
+  { id: 2, name: '部门2', pid: 1 },
+  { id: 3, name: '部门3', pid: 1 },
+  { id: 4, name: '部门4', pid: 3 },
+  { id: 5, name: '部门5', pid: 4 },
+  { id: 6, name: '部门6', pid: 0 }
+]
