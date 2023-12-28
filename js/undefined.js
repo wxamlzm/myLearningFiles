933ad04164754abc36ad155c9924069cd4b6e90e
wxamlzm <zd5024@163.com>
2023-12-28 09:43:06 +0800

    UPDATE：undefined

--- js/undefined.js
+++ js/undefined.js
@@ -1,3 +1,10 @@
+/*
+ * @Author: zd
+ * @Date: 2023-12-07 13:56:03
+ * @LastEditors: zd
+ * @LastEditTime: 2023-12-28 09:42:44
+ * @Description: undefined的居然是可以被重载的
+ */
 // 不同于null字面量，undefined并不恒等于undefined原始值，比如说它可以被“作用域链截胡“
 let undefined = 'bilibili'
 
